b92a68523227cc3eaecd07962b120f6a3dd0f627
prateek <gprateek558@gmail.com>
2020-08-01 04:25:35 -0700

    Updates

---
+++ precache-manifest.e5e78f6426fa44ff6e4002d0a56f6fe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "547f1f2bf87060fa79d5bd1381fbb13e",
+    "url": "/ipod/index.html"
+  },
+  {
+    "revision": "328dc05bc3b9571fafb0",
+    "url": "/ipod/static/css/main.773cb8ae.chunk.css"
+  },
+  {
+    "revision": "4116b5bc817f3bc37844",
+    "url": "/ipod/static/js/2.0d44bc41.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/ipod/static/js/2.0d44bc41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "328dc05bc3b9571fafb0",
+    "url": "/ipod/static/js/main.58fcee91.chunk.js"
+  },
+  {
+    "revision": "e6acf8b982710a9850e8",
+    "url": "/ipod/static/js/runtime-main.59161aa3.js"
+  }
+]);
